a36ed429569b8b5de17448c994ae81089d8ae972
Kauesamartino <kaue_samartino@hotmail.com>
2025-08-20 11:27:15 -0300

    Implement OcorrenciaRepository

---
+++ src/main/java/com/sentinel/api/domain/ocorrencia/OcorrenciaRepository.java
@@ -0,0 +1,6 @@
+package com.sentinel.api.domain.ocorrencia;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Long> {
+}
