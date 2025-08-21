0768328a9628f6ce94d0da807a2c81bb81f644d8
kauetrnt <rm559317@fiap.com.br>
2025-08-21 02:35:50 -0300

    Implement Validations and SpringDocConfigurations

---
+++ src/main/java/com/sentinel/api/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,21 @@
+package com.sentinel.api.infra.springdoc;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Sentinel API")
+                        .description("API Rest da aplicação Sentinel")
+                        .contact(new Contact()
+                                .name("TRNT Team")
+                        ));
+    }
+}
