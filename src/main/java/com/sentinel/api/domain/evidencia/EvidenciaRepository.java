3d50e8ed8096c819492f002ec5fc2fb89948ebe0
kauetrnt <rm559317@fiap.com.br>
2025-09-08 03:06:29 -0300

    fix(evidencia) - remove all content of evidencia

--- src/main/java/com/sentinel/api/domain/evidencia/EvidenciaRepository.java
+++
@@ -1,6 +0,0 @@
-package com.sentinel.api.domain.evidencia;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EvidenciaRepository extends JpaRepository<Evidencia, Long> {
-}
