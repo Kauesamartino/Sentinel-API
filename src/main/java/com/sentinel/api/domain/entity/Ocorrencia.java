92f2ad73fe49b4a0165b9bb5956c4faa80c71191
kauetrnt <rm559317@fiap.com.br>
2025-09-08 13:39:06 -0300

    feat(entity/ocorrencia): delete atualizarInformacoes method

--- src/main/java/com/sentinel/api/domain/entity/Ocorrencia.java
+++ src/main/java/com/sentinel/api/domain/entity/Ocorrencia.java
@@ -4,7 +4,7 @@ import com.sentinel.api.application.usecases.ocorrencia.CreateOcorrenciaInput;
 import com.sentinel.api.domain.enums.Severidade;
 import com.sentinel.api.domain.enums.Status;
 import com.sentinel.api.domain.enums.TipoOcorrencia;
-import com.sentinel.api.interfaces.dto.ocorrencia.DadosAtualizacaoOcorrencia;
+import com.sentinel.api.interfaces.dto.ocorrencia.OcorrenciaUpdateDto;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.*;
@@ -60,20 +60,6 @@ public class Ocorrencia {
         this.tipoOcorrencia = input.tipoOcorrencia();
     }
 
-    public void atualizarInformacoes(@Valid DadosAtualizacaoOcorrencia dados){
-        if(dados.titulo() != null){
-            this.titulo = dados.titulo();
-        }
-        if (dados.descricao() != null){
-            this.descricao = dados.descricao();
-        }
-        if (dados.status() != null){
-            this.status = dados.status();
-        }
-        if (dados.tipoOcorrencia() != null){
-            this.tipoOcorrencia = dados.tipoOcorrencia();
-        }
-    }
 
     public void excluir() {
         this.ativo = false;
