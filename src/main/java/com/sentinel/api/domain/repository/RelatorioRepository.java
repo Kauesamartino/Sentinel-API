380501eac00997b2486e6cb1c408e51683436459
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-09 13:53:54 -0300

    fix(domain/relatorio): remove annotation of RelatorioRepository

--- src/main/java/com/sentinel/api/domain/repository/RelatorioRepository.java
+++ src/main/java/com/sentinel/api/domain/repository/RelatorioRepository.java
@@ -1,9 +1,7 @@
 package com.sentinel.api.domain.repository;
 
 import com.sentinel.api.domain.model.Relatorio;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface RelatorioRepository {
     Relatorio save(Relatorio relatorio);
 }
