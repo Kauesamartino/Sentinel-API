52b55706d13e1a94d56ca515dbb95f4381688ec3
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-09 14:06:12 -0300

    feat(mapper/relatorio): implement domainToOutDto, domainToJpaEntity and jpaEntityToDomain methods

--- src/main/java/com/sentinel/api/interfaces/mapper/RelatorioMapper.java
+++ src/main/java/com/sentinel/api/interfaces/mapper/RelatorioMapper.java
@@ -1,7 +1,9 @@
 package com.sentinel.api.interfaces.mapper;
 
 import com.sentinel.api.domain.model.Relatorio;
+import com.sentinel.api.infrastructure.entity.JpaRelatorioEntity;
 import com.sentinel.api.interfaces.dto.relatorio.RelatorioInDto;
+import com.sentinel.api.interfaces.dto.relatorio.RelatorioOutDto;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Component;
 
@@ -9,5 +11,24 @@ import org.springframework.stereotype.Component;
 public class RelatorioMapper {
     public Relatorio inDtoToDomain(@Valid RelatorioInDto dados) {
         return new Relatorio(dados);
+    }
+
+    public RelatorioOutDto domainToOutDto(Relatorio createdRelatorio) {
+        return new RelatorioOutDto(createdRelatorio);
+    }
+
+    public JpaRelatorioEntity domainToJpaEntity(Relatorio relatorio) {
+        return new JpaRelatorioEntity(relatorio);
+    }
+
+    public Relatorio jpaEntityToDomain(JpaRelatorioEntity savedEntity) {
+        return new Relatorio(
+                savedEntity.getId(),
+                savedEntity.getTitulo(),
+                savedEntity.getDescricao(),
+                savedEntity.getTipoOcorrencia(),
+                savedEntity.getDataInicio(),
+                savedEntity.getDataFim()
+        );
     }
 }
