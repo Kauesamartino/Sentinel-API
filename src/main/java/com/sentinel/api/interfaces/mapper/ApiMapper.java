49a760ce6834fd8fed2896274db0c11696ccc69e
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-09 14:32:07 -0300

    feat(mapper/relatorio): implement method domainToLazyDto in ApiMapper

--- src/main/java/com/sentinel/api/interfaces/mapper/ApiMapper.java
+++ src/main/java/com/sentinel/api/interfaces/mapper/ApiMapper.java
@@ -2,8 +2,10 @@ package com.sentinel.api.interfaces.mapper;
 
 import com.sentinel.api.domain.model.Estacao;
 import com.sentinel.api.domain.model.Ocorrencia;
+import com.sentinel.api.domain.model.Relatorio;
 import com.sentinel.api.interfaces.dto.estacao.EstacaoLazyOutDto;
 import com.sentinel.api.interfaces.dto.ocorrencia.OcorrenciaLazyOutDto;
+import com.sentinel.api.interfaces.dto.relatorio.DadosListagemRelatorios;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,5 +16,9 @@ public class ApiMapper {
 
     public EstacaoLazyOutDto estacaoToLazyDto(Estacao estacao) {
         return new EstacaoLazyOutDto(estacao);
+    }
+
+    public DadosListagemRelatorios domainToLazyDto(Relatorio relatorio) {
+        return new DadosListagemRelatorios(relatorio);
     }
 }
