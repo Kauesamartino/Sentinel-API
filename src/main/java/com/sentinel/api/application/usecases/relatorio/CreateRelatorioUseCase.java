c5f2b65a2b77405bc2dc7a2ebdb4ade9b1bad371
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-09 13:52:20 -0300

    feat(usecase/relatorio): implement logical to save method in CreateRelatorioUseCase

--- src/main/java/com/sentinel/api/application/usecases/relatorio/CreateRelatorioUseCase.java
+++ src/main/java/com/sentinel/api/application/usecases/relatorio/CreateRelatorioUseCase.java
@@ -1,8 +1,24 @@
 package com.sentinel.api.application.usecases.relatorio;
 
+import com.sentinel.api.domain.model.Relatorio;
+import com.sentinel.api.domain.repository.RelatorioRepository;
+import com.sentinel.api.infrastructure.repository.JpaRelatorioRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
 @Component
+@RequiredArgsConstructor
 public class CreateRelatorioUseCase {
 
+    private final RelatorioRepository relatorioRepository;
+
+    @Transactional
+    public Relatorio execute(Relatorio relatorio) {
+        if (relatorio == null) {
+            throw new IllegalArgumentException("Relatorio não pode ser nulo");
+        }
+        return relatorioRepository.save(relatorio);
+    }
 }
