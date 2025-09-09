100128e9c678d016b3221cf438b95f444146f635
kauetrnt <rm559317@fiap.com.br>
2025-09-09 00:20:54 -0300

    feat(domain/cco): add entity CentroControleOperacoes

---
+++ src/main/java/com/sentinel/api/domain/model/CentroControleOperacoes.java
@@ -0,0 +1,13 @@
+package com.sentinel.api.domain.model;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class CentroControleOperacoes {
+    private Long id;
+    private String name;
+
+}
