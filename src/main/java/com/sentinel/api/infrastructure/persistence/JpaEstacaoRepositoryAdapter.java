e7dd6e5f0181fc94f70fe8f86990baa579491432
kauetrnt <rm559317@fiap.com.br>
2025-09-09 00:57:56 -0300

    feat(adapter/estacao): implement save method

--- src/main/java/com/sentinel/api/infrastructure/persistence/JpaEstacaoRepositoryAdapter.java
+++ src/main/java/com/sentinel/api/infrastructure/persistence/JpaEstacaoRepositoryAdapter.java
@@ -1,8 +1,11 @@
 package com.sentinel.api.infrastructure.persistence;
 
+import com.sentinel.api.domain.model.CentroControleOperacoes;
 import com.sentinel.api.domain.model.Estacao;
 import com.sentinel.api.domain.repository.EstacaoRepository;
+import com.sentinel.api.infrastructure.entity.JpaCentroControleOperacoesEntity;
 import com.sentinel.api.infrastructure.entity.JpaEstacaoEntity;
+import com.sentinel.api.infrastructure.repository.JpaCentroControleOperacoesRepository;
 import com.sentinel.api.infrastructure.repository.JpaEstacaoRepository;
 import com.sentinel.api.interfaces.mapper.EstacaoMapper;
 import jakarta.transaction.Transactional;
@@ -14,12 +17,15 @@ import org.springframework.stereotype.Repository;
 public class JpaEstacaoRepositoryAdapter implements EstacaoRepository {
 
     private final JpaEstacaoRepository jpaEstacaoRepository;
+    private final JpaCentroControleOperacoesRepository  jpaCentroControleOperacoesRepository;
     private final EstacaoMapper mapper;
 
     @Transactional
     public Estacao save(Estacao estacao) {
-
-        return null;
+        JpaCentroControleOperacoesEntity cco = jpaCentroControleOperacoesRepository.getReferenceById(estacao.getIdCco());
+        JpaEstacaoEntity entity = mapper.domainToJpaEntity(estacao, cco);
+        JpaEstacaoEntity savedEntity = jpaEstacaoRepository.save(entity);
+        return mapper.jpaEntityToDomain(savedEntity);
     }
 
     @Override
