0f4b15710eab737ede2ceb72e68fec8d13a727a8
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-15 03:08:52 -0300

    feat(exception/global): add ApplicationException

---
+++ src/main/java/com/sentinel/api/application/exceptions/ApplicationException.java
@@ -0,0 +1,11 @@
+package com.sentinel.api.application.exceptions;
+
+public class ApplicationException extends RuntimeException {
+    public ApplicationException(String message) {
+        super(message);
+    }
+
+    public ApplicationException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
