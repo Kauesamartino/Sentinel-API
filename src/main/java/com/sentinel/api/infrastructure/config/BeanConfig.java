f68b47d95a7f0fbcece84c434d2bbf4a3c873d22
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-15 03:12:21 -0300

    feat(config/beanconfig): implement ocorrenciaService and CreateOcorrenciaUseCaseImpl beans

--- src/main/java/com/sentinel/api/infrastructure/config/BeanConfig.java
+++ src/main/java/com/sentinel/api/infrastructure/config/BeanConfig.java
@@ -1,23 +1,39 @@
 package com.sentinel.api.infrastructure.config;
 
 import com.sentinel.api.application.service.CentroControleOperacoesService;
+import com.sentinel.api.application.service.OcorrenciaService;
 import com.sentinel.api.application.usecase.cco.CreateCcoUseCaseImpl;
+import com.sentinel.api.application.usecase.ocorrencia.CreateOcorrenciaUseCaseImpl;
 import com.sentinel.api.domain.repository.CentroControleOperacoesRepository;
+import com.sentinel.api.domain.repository.OcorrenciaRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class BeanConfig {
 
+
+    // Services
     @Bean
     public CentroControleOperacoesService centroControleOperacoesService(CentroControleOperacoesRepository centroControleOperacoesRepository) {
         return new CentroControleOperacoesService(centroControleOperacoesRepository);
     }
 
     @Bean
+    public OcorrenciaService ocorrenciaService(OcorrenciaRepository ocorrenciaRepository) {
+        return new OcorrenciaService(ocorrenciaRepository);
+    }
+
+    // Cco usecases
+    @Bean
     public CreateCcoUseCaseImpl createCcoUseCaseImpl(CentroControleOperacoesService centroControleOperacoesService) {
         return new CreateCcoUseCaseImpl(centroControleOperacoesService);
     }
 
+    // Ocorrencia usecases
+    @Bean
+    public CreateOcorrenciaUseCaseImpl createOcorrenciaUseCaseImpl(OcorrenciaService ocorrenciaService) {
+        return new CreateOcorrenciaUseCaseImpl(ocorrenciaService);
+    }
 
 }
