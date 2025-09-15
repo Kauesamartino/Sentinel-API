997439bce049571106ebbde09fe26b76e9a255fd
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-15 03:13:29 -0300

    feat(service/ocorrencia): add OcorrenciaService

---
+++ src/main/java/com/sentinel/api/application/service/OcorrenciaService.java
@@ -0,0 +1,34 @@
+package com.sentinel.api.application.service;
+
+import com.sentinel.api.application.exceptions.ocorrencia.OcorrenciaDescricaoInvalidaException;
+import com.sentinel.api.application.exceptions.ocorrencia.OcorrenciaSeveridadeInvalidaException;
+import com.sentinel.api.application.exceptions.ocorrencia.OcorrenciaTipoInvalidoException;
+import com.sentinel.api.application.exceptions.ocorrencia.OcorrenciaTituloInvalidoException;
+import com.sentinel.api.domain.model.Ocorrencia;
+import com.sentinel.api.domain.repository.OcorrenciaRepository;
+
+public class OcorrenciaService {
+
+    private final OcorrenciaRepository ocorrenciaRepository;
+
+    public OcorrenciaService(OcorrenciaRepository ocorrenciaRepository) {
+        this.ocorrenciaRepository = ocorrenciaRepository;
+    }
+
+    public Ocorrencia save(Ocorrencia ocorrencia) {
+        if (ocorrencia.getTitulo() == null || ocorrencia.getTitulo().isEmpty()) {
+            throw new OcorrenciaTituloInvalidoException();
+        }
+        if (ocorrencia.getDescricao() == null || ocorrencia.getDescricao().isEmpty()) {
+            throw new OcorrenciaDescricaoInvalidaException();
+        }
+        if (ocorrencia.getSeveridade() == null){
+            throw new OcorrenciaSeveridadeInvalidaException();
+        }
+        if (ocorrencia.getTipoOcorrencia() == null) {
+            throw new OcorrenciaTipoInvalidoException();
+        }
+
+        return ocorrenciaRepository.save(ocorrencia);
+    }
+}
