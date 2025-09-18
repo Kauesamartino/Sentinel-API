89dbb72adafd21848b7f2aba685a7c4fb1838b0a
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-18 01:39:09 -0300

    feat(service/relatorio)  - add RelatorioService  - implement save method to RelatorioService  - add DataValidator  - implement validarInicioMenorQueFim and validarAntecedenciaMinima methods

---
+++ src/main/java/com/sentinel/api/application/validation/DataValidator.java
@@ -0,0 +1,24 @@
+package com.sentinel.api.application.validation;
+
+import com.sentinel.api.application.exceptions.InvalidDateException;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class DataValidator {
+
+    // Valida se inicio < fim
+    public static void validarInicioMenorQueFim(LocalDateTime inicio, LocalDateTime fim) {
+        if (!inicio.isBefore(fim)) {
+            throw new InvalidDateException("Data de início não pode ser depois da data de fim.");
+        }
+    }
+
+    // Valida se inicio está pelo menos 1 hora antes do fim
+    public static void validarAntecedenciaMinima(LocalDateTime inicio, LocalDateTime fim, long horasMinimas) {
+        Duration duracao = Duration.between(inicio, fim);
+        if (duracao.toHours() < horasMinimas) {
+            throw new InvalidDateException("Data de início deve ser no mínimo " + horasMinimas + " hora(s) antes da data de fim.");
+        }
+    }
+}
