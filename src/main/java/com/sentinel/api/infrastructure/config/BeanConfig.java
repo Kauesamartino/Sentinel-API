dd4a91fca34fe01068875199434c71785591c357
Kauesamartino <kaue_samartino@hotmail.com>
2025-09-18 01:40:10 -0300

    feat(bean): implement RelatorioService and CreateRelatorioUseCaseImpl

--- src/main/java/com/sentinel/api/infrastructure/config/BeanConfig.java
+++ src/main/java/com/sentinel/api/infrastructure/config/BeanConfig.java
@@ -2,10 +2,13 @@ package com.sentinel.api.infrastructure.config;
 
 import com.sentinel.api.application.service.CentroControleOperacoesService;
 import com.sentinel.api.application.service.OcorrenciaService;
+import com.sentinel.api.application.service.RelatorioService;
 import com.sentinel.api.application.usecase.cco.CreateCcoUseCaseImpl;
 import com.sentinel.api.application.usecase.ocorrencia.CreateOcorrenciaUseCaseImpl;
+import com.sentinel.api.application.usecase.relatorio.CreateRelatorioUseCaseImpl;
 import com.sentinel.api.domain.repository.CentroControleOperacoesRepository;
 import com.sentinel.api.domain.repository.OcorrenciaRepository;
+import com.sentinel.api.domain.repository.RelatorioRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,6 +27,11 @@ public class BeanConfig {
         return new OcorrenciaService(ocorrenciaRepository);
     }
 
+    @Bean
+    public RelatorioService relatorioService(RelatorioRepository relatorioRepository) {
+        return new RelatorioService(relatorioRepository);
+    }
+
     // Cco usecases
     @Bean
     public CreateCcoUseCaseImpl createCcoUseCaseImpl(CentroControleOperacoesService centroControleOperacoesService) {
@@ -34,6 +42,12 @@ public class BeanConfig {
     @Bean
     public CreateOcorrenciaUseCaseImpl createOcorrenciaUseCaseImpl(OcorrenciaService ocorrenciaService) {
         return new CreateOcorrenciaUseCaseImpl(ocorrenciaService);
+    }
+
+    // Relatorio usecases
+    @Bean
+    public CreateRelatorioUseCaseImpl  createRelatorioUseCaseImpl(RelatorioService relatorioService) {
+        return new CreateRelatorioUseCaseImpl(relatorioService);
     }
 
 }
