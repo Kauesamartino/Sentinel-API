54d10c8abb7ad50d193897ababe5f5fdbcc4f7c6
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-21 11:02:52 -0300

    feat(repository): update OcorrenciaRepositoryAdapter to include camera reference in save method

--- src/main/java/com/sentinel/api/infrastructure/persistence/OcorrenciaRepositoryAdapter.java
+++ src/main/java/com/sentinel/api/infrastructure/persistence/OcorrenciaRepositoryAdapter.java
@@ -2,10 +2,12 @@ package com.sentinel.api.infrastructure.persistence;
 
 import com.sentinel.api.domain.model.Ocorrencia;
 import com.sentinel.api.domain.repository.OcorrenciaRepository;
+import com.sentinel.api.infrastructure.entity.JpaCameraEntity;
 import com.sentinel.api.infrastructure.entity.JpaEstacaoEntity;
 import com.sentinel.api.infrastructure.entity.JpaOcorrenciaEntity;
 import com.sentinel.api.infrastructure.entity.JpaRelatorioEntity;
 import com.sentinel.api.infrastructure.repository.JpaEstacaoRepository;
+import com.sentinel.api.infrastructure.repository.JpaCameraRepository;
 import com.sentinel.api.infrastructure.repository.JpaOcorrenciaRepository;
 import com.sentinel.api.infrastructure.repository.JpaRelatorioRepository;
 import com.sentinel.api.interfaces.mapper.OcorrenciaMapper;
@@ -22,12 +24,14 @@ public class OcorrenciaRepositoryAdapter implements OcorrenciaRepository {
     private final JpaOcorrenciaRepository jpaOcorrenciaRepository;
     private final OcorrenciaMapper mapper;
     private final JpaEstacaoRepository jpaEstacaoRepository;
+    private final JpaCameraRepository jpaCameraRepository;
     private final JpaRelatorioRepository jpaRelatorioRepository;
 
     @Transactional
     public Ocorrencia save(Ocorrencia ocorrencia) {
         JpaEstacaoEntity estacao = jpaEstacaoRepository.getReferenceById(ocorrencia.getIdEstacao());
-        JpaOcorrenciaEntity entity = mapper.domainToJpa(ocorrencia, estacao);
+        JpaCameraEntity camera = jpaCameraRepository.getReferenceById(ocorrencia.getIdCamera());
+        JpaOcorrenciaEntity entity = mapper.domainToJpa(ocorrencia, estacao, camera);
         JpaOcorrenciaEntity savedEntity = jpaOcorrenciaRepository.save(entity);
         return mapper.jpaEntityToDomain(savedEntity);
     }
