a1fa5ca2e90cf41aab1d220a88f5a6125cadce1b
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-23 20:45:21 -0300

    refactor(controller): update EstacaoController to use new use case implementations and integrate Cco data

--- src/main/java/com/sentinel/api/interfaces/controller/EstacaoController.java
+++ src/main/java/com/sentinel/api/interfaces/controller/EstacaoController.java
@@ -1,9 +1,10 @@
 package com.sentinel.api.interfaces.controller;
 
-import com.sentinel.api.application.usecase.estacao.CreateEstacaoUseCaseImpl;
-import com.sentinel.api.application.usecase.estacao.DeleteEstacaoUseCaseImpl;
-import com.sentinel.api.application.usecase.estacao.GetEstacaoUseCaseImpl;
-import com.sentinel.api.application.usecase.estacao.GetEstacoesUseCaseImpl;
+import com.sentinel.api.application.usecase.cco.GetCcoUseCase;
+import com.sentinel.api.application.usecase.estacao.CreateEstacaoUseCase;
+import com.sentinel.api.application.usecase.estacao.DeleteEstacaoUseCase;
+import com.sentinel.api.application.usecase.estacao.GetEstacaoUseCase;
+import com.sentinel.api.application.usecase.estacao.GetEstacoesUseCase;
 import com.sentinel.api.domain.model.Estacao;
 import com.sentinel.api.interfaces.dto.estacao.EstacaoInDto;
 import com.sentinel.api.interfaces.dto.estacao.EstacaoOutDto;
@@ -11,7 +12,6 @@ import com.sentinel.api.interfaces.dto.estacao.EstacaoLazyOutDto;
 import com.sentinel.api.interfaces.mapper.ApiMapper;
 import com.sentinel.api.interfaces.mapper.EstacaoMapper;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -23,21 +23,27 @@ import java.net.URI;
 
 @RestController
 @RequestMapping("estacoes")
-@RequiredArgsConstructor
 public class EstacaoController {
 
-    private final EstacaoMapper mapper;
-    private final CreateEstacaoUseCaseImpl createEstacaoUseCaseImpl;
-    private final GetEstacoesUseCaseImpl getEstacoesUseCaseImpl;
-    private final ApiMapper apiMapper;
-    private final GetEstacaoUseCaseImpl getEstacaoUseCaseImpl;
-    private final DeleteEstacaoUseCaseImpl deleteEstacaoUseCaseImpl;
+    private final CreateEstacaoUseCase createEstacaoUseCase;
+    private final GetEstacoesUseCase getEstacoesUseCase;
+    private final GetEstacaoUseCase getEstacaoUseCase;
+    private final DeleteEstacaoUseCase deleteEstacaoUseCase;
+    private final GetCcoUseCase getCcoUseCase;
+
+    public EstacaoController(CreateEstacaoUseCase createEstacaoUseCase, GetEstacoesUseCase getEstacoesUseCase, GetEstacaoUseCase getEstacaoUseCase, DeleteEstacaoUseCase deleteEstacaoUseCase, GetCcoUseCase getCcoUseCase) {
+        this.createEstacaoUseCase = createEstacaoUseCase;
+        this.getEstacoesUseCase = getEstacoesUseCase;
+        this.getEstacaoUseCase = getEstacaoUseCase;
+        this.deleteEstacaoUseCase = deleteEstacaoUseCase;
+        this.getCcoUseCase = getCcoUseCase;
+    }
 
     @PostMapping
     public ResponseEntity<EstacaoOutDto> cadastrar(@RequestBody @Valid EstacaoInDto dados, UriComponentsBuilder uriBuilder){
-        Estacao estacao = mapper.inDtoToDomain(dados);
-        Estacao createdEstacao = createEstacaoUseCaseImpl.execute(estacao);
-        EstacaoOutDto estacaoOutDto = mapper.domainToOutDto(createdEstacao);
+        Estacao estacao = EstacaoMapper.inDtoToDomain(dados);
+        Estacao createdEstacao = createEstacaoUseCase.execute(estacao);
+        EstacaoOutDto estacaoOutDto = EstacaoMapper.domainToOutDto(createdEstacao, getCcoUseCase.execute(createdEstacao.getIdCco()));
         URI uri = uriBuilder.path("/estacoes/{id}").buildAndExpand(estacao.getId()).toUri();
         return ResponseEntity.created(uri).body(estacaoOutDto);
     }
@@ -47,21 +53,21 @@ public class EstacaoController {
                                                           @RequestParam(name = "pageNumber", required = false, defaultValue = "0") Integer pageNumber){
 
         Pageable pageable = PageRequest.of(pageNumber, pageSize);
-        Page<Estacao> domainPage = getEstacoesUseCaseImpl.execute(pageable);
-        Page<EstacaoLazyOutDto> dto = domainPage.map(apiMapper::estacaoToLazyDto);
+        Page<Estacao> domainPage = getEstacoesUseCase.execute(pageable);
+        Page<EstacaoLazyOutDto> dto = domainPage.map(ApiMapper::estacaoToLazyDto);
         return ResponseEntity.ok(dto);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<EstacaoOutDto> detalhar(@PathVariable Long id){
-        Estacao estacao = getEstacaoUseCaseImpl.execute(id);
-        EstacaoOutDto estacaoOutDto = mapper.domainToOutDto(estacao);
+        Estacao estacao = getEstacaoUseCase.execute(id);
+        EstacaoOutDto estacaoOutDto = EstacaoMapper.domainToOutDto(estacao, getCcoUseCase.execute(estacao.getIdCco()));
         return ResponseEntity.ok(estacaoOutDto);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletar(@PathVariable Long id){
-        deleteEstacaoUseCaseImpl.execute(id);
+        deleteEstacaoUseCase.execute(id);
         return ResponseEntity.noContent().build();
     }
 }
