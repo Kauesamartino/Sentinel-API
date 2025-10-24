d873f19d7ca58381ce1bebde93934e7a440b7860
Kauesamartino <kaue_samartino@hotmail.com>
2025-10-24 09:53:12 -0300

    feat(config): add ControllersConfig for OcorrenciaController bean configuration

---
+++ src/main/java/com/sentinel/api/infrastructure/config/ControllersConfig.java
@@ -0,0 +1,31 @@
+package com.sentinel.api.infrastructure.config;
+
+import com.sentinel.api.application.usecase.camera.GetCameraUseCase;
+import com.sentinel.api.application.usecase.cco.GetCcoUseCase;
+import com.sentinel.api.application.usecase.estacao.GetEstacaoUseCase;
+import com.sentinel.api.application.usecase.ocorrencia.*;
+import com.sentinel.api.interfaces.controller.OcorrenciaController;
+import com.sentinel.api.interfaces.controller.OcorrenciaControllerImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ControllersConfig {
+
+    @Bean
+    public OcorrenciaController ocorrenciaController(
+            CreateOcorrenciaUseCase createOcorrenciaUseCase, UpdateOcorrenciaUseCase updateOcorrenciaUseCase,
+            DeleteOcorrenciaUseCase deleteOcorrenciaUseCase, GetOcorrenciasUseCase getOcorrenciasUseCase,
+            GetOcorrenciaUseCase getOcorrenciaUseCase, GetOcorrenciasRelatorioUseCase getOcorrenciasRelatorioUseCase,
+            GetOcorrenciasAtivoFalseUseCase getOcorrenciasAtivoFalseUseCase, PatchOcorrenciaAtivoUseCase patchOcorrenciaAtivoUseCase,
+            GetCameraUseCase getCameraUseCase, GetEstacaoUseCase getEstacaoUseCase, GetCcoUseCase getCcoUseCase
+    ) {
+        return new OcorrenciaControllerImpl(
+                createOcorrenciaUseCase, updateOcorrenciaUseCase, deleteOcorrenciaUseCase,
+                getOcorrenciasUseCase, getOcorrenciaUseCase, getOcorrenciasRelatorioUseCase,
+                getOcorrenciasAtivoFalseUseCase, patchOcorrenciaAtivoUseCase,
+                getCameraUseCase, getEstacaoUseCase, getCcoUseCase
+        );
+    }
+
+}
